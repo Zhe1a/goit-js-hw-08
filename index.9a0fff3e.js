ce194bd9c6f5ccf137d07206ad42596409aceff1
Zhe1a <108029808+Zhe1a@users.noreply.github.com>
2022-09-28 21:35:08 +0000

    Deploying to gh-pages from @ Zhe1a/goit-js-hw-08@388821787f6ac35823976deeeb89e986f71da26a 🚀

--- index.9a0fff3e.js
+++
@@ -1,2 +0,0 @@
-var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},r={},o=e.parcelRequire4c75;null==o&&((o=function(e){if(e in n)return n[e].exports;if(e in r){var o=r[e];delete r[e];var i={id:e,exports:{}};return n[e]=i,o.call(i.exports,i,i.exports),i.exports}var l=new Error("Cannot find module '"+e+"'");throw l.code="MODULE_NOT_FOUND",l}).register=function(e,n){r[e]=n},e.parcelRequire4c75=o),o("gu8sf");
-//# sourceMappingURL=index.9a0fff3e.js.map
