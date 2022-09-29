f5b4d0b9496c6493b8e690d250e67a9c68024ec3
Zhe1a <mgr58006@gmail.com>
2022-09-29 22:25:07 +0300

    fixs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,5 +11,11 @@ player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay ({ seconds }) {
     localStorage.setItem('videoplayer-current-time', seconds)
 }
-
+function SETLocalStorage() {
+    if(!localStorage.getItem('videoplayer-current-time')){
+      return
+    }
     player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+}
+SETLocalStorage()
+
